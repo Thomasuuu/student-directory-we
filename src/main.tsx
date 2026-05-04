a59468bcb23ed32f6fe95be6d6aef59131035e5a
Thomas <129727824+Thomasuuu@users.noreply.github.com>
2026-05-04 18:47:32 +0000

    Generated by Spark: Fix all reported errors.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { ErrorBoundary } from "react-error-boundary";
 import "@github/spark/spark"
+import { Toaster } from '@/components/ui/sonner'
 
 import App from './App.tsx'
 import { ErrorFallback } from './ErrorFallback.tsx'
@@ -10,7 +12,10 @@ import "./styles/theme.css"
 import "./index.css"
 
 createRoot(document.getElementById('root')!).render(
-  <ErrorBoundary FallbackComponent={ErrorFallback}>
-    <App />
-   </ErrorBoundary>
+  <React.StrictMode>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <App />
+      <Toaster />
+    </ErrorBoundary>
+  </React.StrictMode>
 )
